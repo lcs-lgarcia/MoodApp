22120fef3db24cb7ce227e9e7c6cae191ccbd3e4
Lucas Garcia <lgarciaalonso@lcs.on.ca>
2023-04-07 21:21:53 -0400

    Initial Commit

---
+++ MoodApp/MoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodAppApp.swift
+//  MoodApp
+//
+//  Created by Lucas García on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
